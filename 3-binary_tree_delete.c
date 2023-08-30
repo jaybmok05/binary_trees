71bc6158c20bc4dbfd192deef23a897bd6ca0352
jabulane <jabumokoena85@gmail.com>
2023-08-30 02:29:20 -0700

    adding task3: 3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - free an entire tree using recursion it free the node
+ * when the node left and right are NULL
+ * @tree: tree to free
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
+	return;
+}
