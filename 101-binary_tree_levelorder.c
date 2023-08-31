77733bfd4b2c0893b23011e1198349655094c12a
jabulane <jabumokoena85@gmail.com>
2023-08-31 14:43:26 -0700

    adding advanced tasks

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,65 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - traverst a binary tree using level-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t level = 1, maxlevel;
+
+	if (tree != NULL || func != NULL)
+	{
+		maxlevel = binary_tree_height(tree) + 1;
+		do {
+			btlo_helper(tree, func, level);
+			level++;
+		} while (level <= maxlevel);
+	}
+}
+
+/**
+ * btlo_helper - goes through a binary tree using post-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
+ * @level: the level of the tree to call func upon
+ */
+
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
+	}
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ * Return: height of the tree 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+}
